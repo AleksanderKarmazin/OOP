fcd582c215dc52b454882ecc90ae7b25fe610e71
karmazin <kubartphil@gmail.com>
2021-06-11 10:55:23 +0300

    Minin patterns

---
+++ 5_module_from_class/1.js
@@ -0,0 +1,12 @@
+const denis = {
+    name: 'Denis',
+    age:30
+ }
+
+// При вызове функций конструкторов
+//  создаётся какой-то объект
+
+// ВТРОЕНЫЕ 
+
+const str = new String('Hellow')
+console.log(str);
